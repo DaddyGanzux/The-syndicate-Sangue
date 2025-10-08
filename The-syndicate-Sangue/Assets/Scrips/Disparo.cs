c2463b378c7e1194262f951629dbdc3b3be05bb5
sev_sgk <Clevereratom120@gmail.com>
2025-10-08 14:30:09 -0600

    🚧
    Meu

---
+++ The-syndicate-Sangue/Assets/Scrips/Disparo.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Disparo : MonoBehaviour
+{
+    //Diparo de Primera persona
+    /*
+    [SerializeField] private GameObject proyectil;// Prefab del proyectil a instanciar
+    [SerializeField] private Transform puntoDisparo;// Punto desde donde se disparará el proyectil
+    [SerializeField] private float fuerzaDisparo = 700f;// Fuerza con la que se disparará el proyectil
+    public void Disparar()// Metodo para disparar el proyectil
+    {
+        GameObject nuevoProyectil = Instantiate(proyectil, puntoDisparo.position, puntoDisparo.rotation);
+        Rigidbody rb = nuevoProyectil.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(puntoDisparo.forward * fuerzaDisparo);
+        }
+    }
+    */
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
