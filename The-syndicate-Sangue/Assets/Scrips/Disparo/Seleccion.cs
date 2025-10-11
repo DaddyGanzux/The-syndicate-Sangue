c12a77fa81149d3bada375253a64d14bb3efb96d
sev_sgk <Clevereratom120@gmail.com>
2025-10-10 19:00:00 -0600

    🚧
    implemnetando el sistema de disparo

--- The-syndicate-Sangue/Assets/Scrips/Disparo/Seleccion.cs
+++ The-syndicate-Sangue/Assets/Scrips/Disparo/Seleccion.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Seleccion : MonoBehaviour, IPointerClickHandler
+//Hacer una interfaz para clikear, y que el objeto quede seleccionado. Hacer que el objeto seleccionado cambie de color y click, desclick
 {
     GameObject objetoSeleccionado;
 
@@ -9,6 +10,7 @@ public class Seleccion : MonoBehaviour, IPointerClickHandler
     {
         GameObject obj = eventData.pointerPress; // El objeto sobre el que hiciste clic
         // Aquí puedes manejar el evento de clic
+        Debug.Log(eventData);
         Debug.Log("Objeto clickeado: " + gameObject.name);
     }
 
