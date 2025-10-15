58a7c6853bebd376d8bf0ffaa56dee26ae7d0ff9
sev_sgk <Clevereratom120@gmail.com>
2025-10-14 22:06:24 -0600

    🚧
    Haciendo el mesh para poder mover al enemigo

---
+++ The-syndicate-Sangue/Assets/Scrips/Disparo/EnemyController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("Enemy Settings")]
+    [SerializeField] private int health = 3;
+    public float radius = 0.5f;
+    public GameObject Center;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
