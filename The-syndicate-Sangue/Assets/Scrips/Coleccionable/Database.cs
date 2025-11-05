15f7c6839328e2e790edabb3bc90b564d5978af7
DaddyGanzux <Garm0607@gmail.com>
2025-11-05 09:16:44 -0600

    🚧

---
+++ The-syndicate-Sangue/Assets/Scrips/Coleccionable/Database.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataBase", menuName = "Inventory/New Database", order = 1)]
+public class Database : ScriptableObject
+{
+    [System.Serializable]
+
+    public struct InventoryItem
+    {
+        public string name; // Nombre del coleccionable
+        public int ID; // Numero asignado
+        public Sprite icon; // Icono del coleccionable que se mostrara em el menu
+        //public Type type;
+        //public bool acumulable;
+        //public int maxStack;
+        public string description; // Descripcion del objeto
+        //public BaseItem item;
+    }
+
+    /*public enum Type
+    {
+        consumible,
+        equipable
+    }¨*/
+
+    public InventoryItem[] dataBase;
+
+    private void OnValidate() // Se llama cuando se carga el scriptable object y cuando este cambia en el Inspector
+    {
+        if(dataBase != null)
+        {
+            for(int i = 0;  i < dataBase.Length; i++)
+            {
+                if (dataBase[i].ID != i)
+                {
+                    dataBase[i].ID = i;
+                }
+            }
+        }
+    }
+}
